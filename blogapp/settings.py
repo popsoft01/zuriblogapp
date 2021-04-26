5310d2992e52c5b2a76b03c7ee506dc73b9b530f
popsoft01 <popoolatunde52@gmail.com>
2021-04-26 23:46:27 +0100

    Heroku config

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+)$=24(ng0&d1imrrxw*f*lpjxs^l))_=jbtuhk%=$i9#=a$$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
