af47c1eef6c0c50b1ecfd4faa0641da24e5fc8b9
popsoft01 <popoolatunde52@gmail.com>
2021-09-28 15:53:11 +0100

    still in progress

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (CreateView, UpdateView, DeleteView)
+
 from .models import Post
-from django.urls import reverse
 
 
 # Create your views here
